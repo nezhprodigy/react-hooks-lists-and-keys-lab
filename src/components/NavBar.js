f641daa54f391547003e82eeddb0dadaa2c047d3
alex <alex.musyoki@student.moringaschool.com>
2023-11-23 20:13:06 +0300

    Completed assignments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksNavBar = links.map((link) => {
+    return <a key={link} href={`#${link}`}> {link}</a>
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>{linksNavBar}</nav>
+  );
 }
 
 export default NavBar;
